e1f37472c217984231222de0dde43d84f2c2475e
ArnautGheorghe <agigi94@yahoo.com>
2024-01-30 17:50:44 +0200

    update ex1 pas 4

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,15 +1,72 @@
 import styles from "./FeedbackOptions.modules.css";
-
+import { React, useState, setState } from 'react';
 import PropTypes from 'prop-types';
+import Statistics from "components/Statistics/Statistics";
+import Notifications from "components/Notifications/Notifications";
 
-const FeedbackOptions = () => {
+
+
+const FeedbackOptions = ({ state }) => {
+    
+
+      const [counter, setCounter] = useState(state.good);
+
+      //increase counter
+      const increase = () => {
+        setCounter(count => count + 1);
+    };
+    
+const [counterneutral, setCounterneutral] = useState(state.neutral);
+
+//increase counter
+const increaseneutral = () => {
+  setCounterneutral(count => count + 1);
+};
+
+const [counterbad, setCounterbad] = useState(state.bad);
+
+//increase counter
+const increasebad = () => {
+  setCounterbad(count => count + 1);
+};
+
+   
+    const state2 = {
+        good: counter,
+        bad: counterbad,
+        neutral:counterneutral
+    }
+       
+
+  
+
+
   return (
-    <div >
-      
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
+    <div>
+      <button type="button" onClick={increase}>
+        Good
+      </button>
+      <button type="button" onClick={increaseneutral}>
+        Neutral
+      </button>
+      <button type="button" onClick={increasebad}>
+        Bad
+      </button>
       <br />
+      {state2.good !== 0 || state2.neutral !== 0 || state2.bad !== 0 ? (
+        <Statistics
+          good={state2.good}
+          neutral={state2.neutral}
+          bad={state2.bad}
+          total={state2.good + state2.neutral + state2.bad}
+          positivePercentage={(
+            (state2.good * 100) /
+            (state2.good + state2.neutral + state2.bad)
+          ).toFixed()}
+        />
+      ) : (
+        <Notifications message="There is no feedback" />
+      )}
     </div>
   );
 };
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,31 +5,17 @@ import Notifications from "components/Notifications/Notifications";
 
 const Section = ({title}) => {
     const state = {
-      good: 3,
-      neutral: 2,
-      bad: 2,
+      good: 0,
+      neutral: 0,
+      bad: 0,
     };
     return (
       <div>
         <h2>{title}</h2>
-            <FeedbackOptions />
-            
-        {(state.good !== 0 ||
-          state.neutral !== 0 ||
-                (state.bad !== 0)
-          
-                ? <Statistics
-          good={state.good}
-          neutral={state.neutral}
-          bad={state.bad}
-          total={state.good + state.neutral + state.bad}
-          positivePercentage={(
-            (state.good * 100) /
-            (state.good + state.neutral + state.bad)
-          ).toFixed()}
-                />
-                
-                : <Notifications message="There is no feedback" />)}
+        <FeedbackOptions
+          state={state}
+        />
+
         
       </div>
     );
